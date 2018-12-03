d8e988a5918f5896bd81c9e22eb210ed99e65633
Kaellz Dev <kaellzdev@gmail.com>
2018-12-03 08:57:18 -0200

    was generated the basis of the project

---
+++ Core.Tools.Infra/Features/Configs/ConfigRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Tools.Domain.Features.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Tools.Infra.Features.Configs
+{
+    public class ConfigRepository : IConfigRepository
+    {
+    }
+}
