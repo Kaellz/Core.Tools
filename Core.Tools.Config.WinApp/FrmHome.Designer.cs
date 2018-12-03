becb2b82d564e134e1780fb6f466e3922fd4c9bf
Kaellz Dev <kaellzdev@gmail.com>
2018-12-03 08:29:57 -0200

    first commit

---
+++ Core.Tools.Config.WinApp/FrmHome.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Core.Tools.Config.WinApp
+{
+    partial class FrmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this._btnNext = new System.Windows.Forms.Button();
+            this._btnPrev = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(604, 551);
+            this.panel1.TabIndex = 0;
+            // 
+            // _btnNext
+            // 
+            this._btnNext.Location = new System.Drawing.Point(520, 558);
+            this._btnNext.Name = "_btnNext";
+            this._btnNext.Size = new System.Drawing.Size(75, 23);
+            this._btnNext.TabIndex = 1;
+            this._btnNext.Text = "Next";
+            this._btnNext.UseVisualStyleBackColor = true;
+            // 
+            // _btnPrev
+            // 
+            this._btnPrev.Location = new System.Drawing.Point(12, 558);
+            this._btnPrev.Name = "_btnPrev";
+            this._btnPrev.Size = new System.Drawing.Size(75, 23);
+            this._btnPrev.TabIndex = 2;
+            this._btnPrev.Text = "Prev";
+            this._btnPrev.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 587);
+            this.Controls.Add(this._btnPrev);
+            this.Controls.Add(this._btnNext);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button _btnNext;
+        private System.Windows.Forms.Button _btnPrev;
+    }
+}
+
